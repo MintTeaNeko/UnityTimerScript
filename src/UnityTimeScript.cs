b1e9a8b167778efdc6758ab3983a7c97dd43c666
MintTeaNeko <43279771+MintTeaNeko@users.noreply.github.com>
2022-12-01 18:05:18 +0200

    Create UnityTimeScript.cs

---
+++ src/UnityTimeScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityTimerScript : MonoBehaviour
+{
+    public float Duration;
+    public bool Running;
+    private float _timer;
+
+    public delegate void onFlagReset();
+    public event onFlagReset OnFlagReset;
+
+    private void RaiseOnFlagReset()
+    {
+        if (OnFlagReset != null) OnFlagReset();
+    }
+
+    private void Start()
+    {
+        Running = false;
+        _timer = 0f;
+    }
+
+    private void Update()
+    {
+        if (Running)
+        {
+            _timer += Time.deltaTime;
+            if (_timer > Duration)
+            {
+                _timer = 0f;
+                Running = false;
+                RaiseOnFlagReset();
+            }
+        }
+    }
+}
